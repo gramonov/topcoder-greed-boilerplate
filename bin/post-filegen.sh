cbb252f7f5ed2dd0bc8b763fa3ae94499dfd9f59
George Ramonov <georgy.ramonov@gmail.com>
2015-11-29 15:51:37 -0800

    Fix comments for post-filegen script

--- bin/post-filegen.sh
+++ bin/post-filegen.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Opens created problem file in emacs and its description in browser
-# Remember that Greed passes full path of the file PROBLEM_NAME.script
-# so we need to parse it first and remove ".script" extension.
+# Opens created problem file in emacs and its description in chromium
 problem_dir=${1%/*}
 problem_name=${2}
 
